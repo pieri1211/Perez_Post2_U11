15256cbcbe5b3ae6ab6b0168fd20bbc958039082
pieri1211 <mariapierina12@hotmail.com>
2026-05-12 12:24:25 -0500

    Falto uno de domain

---
+++ src/main/java/com/universidad/pedidoservice/domain/Producto.java
@@ -0,0 +1,19 @@
+package com.universidad.pedidoservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+}
